03d4e82ce33ad331d142a6034db16b007cf854da
Руслан Рогозин <wether@mail2000.ru>
2022-04-26 02:39:42 +0700

    v 1.0

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,13 +10,11 @@ using UnityEngine.Serialization;
 public class Player : MonoBehaviour
 {
     public UnitParameters PlayerParameters;
-
-    private LineRenderer _lineRenderer;
+ 
     private float _speedRotate;
     private float _speed;
     private float _speedFiring;
     private float _speedProjectile;
-    private int counter = 5;
 
     private Rigidbody2D _rigidbody2D;
     private Launcher _launcher;
@@ -27,8 +25,7 @@ public class Player : MonoBehaviour
         _speedRotate = PlayerParameters.SpeedRotate;
         _speedFiring = PlayerParameters.SpeedFiring;
         _speedProjectile = PlayerParameters.SpeedProjectile;
-
-        _lineRenderer = GetComponent<LineRenderer>();
+        
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _launcher = new Launcher(GameObject.FindGameObjectsWithTag("Projectile"), _speedFiring,
             _speedProjectile);
@@ -52,30 +49,5 @@ public class Player : MonoBehaviour
         {
             StartCoroutine(_launcher.Shot(transform.GetChild(0)));
         }
-        _lineRenderer.SetPosition(0, transform.position);
-        var from = transform.position;
-        var to = transform.up;
-        while (Raycast(from, to, out var hit))
-        {
-            counter--;
-            _lineRenderer.SetPosition(5 - counter, hit.point);
-            if (hit.collider.name.Split(' ')[0] == "Wall")
-            {
-                from = hit.point;
-                to = Vector2.Reflect(to, hit.normal);
-            }
-
-            if (counter <= 0)
-                break;
-        }
-
-        counter = 5;
-    }
-
-    public bool Raycast(Vector3 pos, Vector3 dir, out RaycastHit2D hit2D)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(pos, dir, 50f);
-        hit2D = hit;
-        return hit;
     }
 }
